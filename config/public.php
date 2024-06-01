4fb381746bc502dea9efe30a5f8e622190db7a71
Andrey Helldar <helldar@dragon-code.pro>
2024-06-01 03:37:36 +0300

    First Initial

---
+++ config/public.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+use LaravelLang\LocaleList\Locale;
+
+return [
+    /*
+     * Determines what type of files to use when updating language files.
+     *
+     * `true` means inline files will be used.
+     * `false` means that default files will be used.
+     *
+     * For example, the difference between them can be seen here:
+     *
+     * The :attribute must be accepted. // default
+     * This field must be accepted.     // inline
+     *
+     * By default, `false`.
+     */
+
+    'inline' => (bool)env('LOCALIZATION_INLINE', false),
+
+    /*
+     * Do arrays need to be aligned by keys before processing arrays?
+     *
+     * By default, true
+     */
+
+    'align' => (bool)env('LOCALIZATION_ALIGN', true),
+
+    /*
+     * This option determines the mechanism for converting translation
+     * keys into a typographic version.
+     *
+     * For example,
+     * for `false`: "It's super-configurable... -- just like this one!"
+     * for `true`: “It’s super-configurable… – just like this one!”
+     *
+     * By default, false
+     */
+
+    'smart_punctuation' => [
+        'enable' => (bool)env('LOCALIZATION_SMART_ENABLED', false),
+
+        'common' => [
+            'double_quote_opener' => '“',
+            'double_quote_closer' => '”',
+            'single_quote_opener' => '‘',
+            'single_quote_closer' => '’',
+        ],
+
+        'locales' => [
+            Locale::French->value => [
+                'double_quote_opener' => '«&nbsp;',
+                'double_quote_closer' => '&nbsp;»',
+                'single_quote_opener' => '‘',
+                'single_quote_closer' => '’',
+            ],
+
+            Locale::Russian->value => [
+                'double_quote_opener' => '«',
+                'double_quote_closer' => '»',
+                'single_quote_opener' => '‘',
+                'single_quote_closer' => '’',
+            ],
+
+            Locale::Ukrainian->value => [
+                'double_quote_opener' => '«',
+                'double_quote_closer' => '»',
+                'single_quote_opener' => '‘',
+                'single_quote_closer' => '’',
+            ],
+
+            Locale::Belarusian->value => [
+                'double_quote_opener' => '«',
+                'double_quote_closer' => '»',
+                'single_quote_opener' => '‘',
+                'single_quote_closer' => '’',
+            ],
+        ],
+    ],
+
+    /*
+     * The language codes chosen for the files in this repository may not
+     * match the preferences for your project.
+     *
+     * Specify here mappings of localizations with your project.
+     */
+
+    'aliases' => [
+        // \LaravelLang\LocaleList\Locale::German->value => 'de-DE',
+        // \LaravelLang\LocaleList\Locale::GermanSwitzerland->value => 'de-CH',
+    ],
+];
