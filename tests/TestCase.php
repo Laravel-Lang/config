c81ce14cc24358c80707e02f84ada6bd1ab2adf6
Andrey Helldar <helldar@dragon-code.pro>
2025-05-06 20:40:02 +0300

    Renamed `ServiceProvider` with `LaravelLangConfigServiceProvider`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use LaravelLang\Config\ServiceProvider;
+use LaravelLang\Config\LaravelLangConfigServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app): array
     {
-        return [ServiceProvider::class];
+        return [LaravelLangConfigServiceProvider::class];
     }
 }
